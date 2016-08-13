44f78f69a06a6b80852317fe9ba8d9bb68855eaa
zhangyinyue <1016317391@qq.com>
2016-08-13 22:43:33 +0800

    实现整型数组归并排序

--- src/com/zyy/sort/MergeSort.java
+++ src/com/zyy/sort/MergeSort.java
@@ -8,5 +8,54 @@ package com.zyy.sort;
  */
 public class MergeSort {
 
+	private int[] source;
+	
+	/**
+	 * 合并比较
+	 * @param source
+	 * @param first
+	 * @param last
+	 * @param sourceSort
+	 */
+	private void merge (int[] source, int first,int mid, int last, int[] sourceSort){
+		
+		int i = first, j = mid+1, k = 0;
+		while(i <= mid && j <= last){
+			if(source[i] < source[j]){
+				sourceSort[k++] = source[i];
+				i++;
+			}else{
+				sourceSort[k++] = source[j];
+				j++;
+			}
+		}
+		while( i <= mid){
+			sourceSort[k++] = source[i++];
+		}
+		while(j <= last){
+			sourceSort[k++] = source[j++];
+		}
+		
+		for(int l = 0; l < k; l++){
+			source[first+l] = sourceSort[l];
+		}
+	}
+	
+	/**
+	 * 递归分组
+	 * @param source
+	 * @param first
+	 * @param last
+	 * @param sourceSort
+	 */
+	public void mergeSpil(int[] source, int first, int last, int[] sourceSort){
+		int mid = (first + last) / 2;
+		if(first >= last){
+			return ;
+		}
+		mergeSpil(source, first, mid, sourceSort);
+		mergeSpil(source, mid+1, last, sourceSort);
+		merge(source, first,mid, last, sourceSort);
+	}
 	
 }
--- src/com/zyy/test/MergeSortTest.java
+++ src/com/zyy/test/MergeSortTest.java
@@ -1,4 +1,7 @@
 package com.zyy.test;
+
+import com.zyy.sort.MergeSort;
+
 /**
  * 归并排序测试类
  * @author  zyy E-mail: 1016317391@qq.com
@@ -9,8 +12,15 @@ package com.zyy.test;
 public class MergeSortTest {
 
 	public static void main(String[] args) {
-		System.out.println("zhangyinyueshidatiancai");
-
+		//System.out.println("zhangyinyueshidatiancai");
+		MergeSort mergeSort = new MergeSort();
+		int[] source = new int[] {4,3,8,6,1,9,0,5,2};
+		int[] sourceSort = new int[source.length];
+		mergeSort.mergeSpil(source, 0, source.length-1, sourceSort);
+		for(int i = 0 ; i < source.length; i++){
+			System.out.print(source[i]+" ");
+		}
+		
 	}
 	
 }
