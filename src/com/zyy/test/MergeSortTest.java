c8bb32e7c30f0e454f1b7d0fc04db27af625b3e9
zhangyinyue <1016317391@qq.com>
2016-08-13 21:18:37 +0800

    测试主分支跟trunk分支冲突问题

--- src/com/zyy/test/MergeSortTest.java
+++ src/com/zyy/test/MergeSortTest.java
@@ -9,7 +9,7 @@ package com.zyy.test;
 public class MergeSortTest {
 
 	public static void main(String[] args) {
-		System.out.println("zhang");
+		System.out.println("zhangyinyueshidatiancai");
 	}
 	
 }
